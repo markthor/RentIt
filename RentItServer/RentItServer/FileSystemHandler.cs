62ef19ed00f0e2ab877f52ad8158430bcac4b9e2
TheGreenDude <alu_cola@hotmail.com>
2013-02-22 14:38:23 +0100

    Re added classes in RentItServer

---
+++ RentItServer/RentItServer/FileSystemHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentItServer
+{
+    public class FileSystemHandler
+    {
+        //Singleton instance of the class
+        public static FileSystemHandler _instance;
+
+        /// <summary>
+        /// Private to ensure local instantiation.
+        /// </summary>
+        private FileSystemHandler()
+        {
+        }
+
+        /// <summary>
+        /// Accessor method to access the only instance of the class
+        /// </summary>
+        /// <returns>The singleton instance of the class</returns>
+        public static FileSystemHandler GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FileSystemHandler();
+            }
+            return _instance;
+        }
+    }
+}
