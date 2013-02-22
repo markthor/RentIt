143c5b32e8a33a18ba10365fa6715fdaab1b4ee3
Mark Thorhauge <markthorh@gmail.com>
2013-02-22 11:41:21 +0100

    added classes for server

---
+++ RentItServer/RentItServer/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RentItServer
+{
+    class Controller
+    {
+        //Singleton instance of the class
+        public static Controller _instance;
+        //Data access object for database IO
+        public static DAO _dao = DAO.GetInstance();
+        //Responsible for choosing the next track
+        public static TrackPrioritizer _trackPrioritizer = TrackPrioritizer.GetInstance();
+        //Data access object for file system IO
+        public static FileSystemHandler _fileSystemHandler = FileSystemHandler.GetInstance();
+
+        /// <summary>
+        /// Private to ensure local instantiation.
+        /// </summary>
+        private Controller()
+        {
+        }
+
+        /// <summary>
+        /// Accessor method to access the only instance of the class
+        /// </summary>
+        /// <returns>The singleton instance of the class</returns>
+        public static Controller GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Controller();
+            }
+            return _instance;
+        }
+    }
+}
---
+++ RentItServer/RentItServer/TrackPrioritizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RentItServer
+{
+    class TrackPrioritizer
+    {
+        //Singleton instance of the class
+        public static TrackPrioritizer _instance;
+
+        /// <summary>
+        /// Private to ensure local instantiation.
+        /// </summary>
+        private TrackPrioritizer()
+        {
+        }
+
+        /// <summary>
+        /// Accessor method to access the only instance of the class
+        /// </summary>
+        /// <returns>The singleton instance of the class</returns>
+        public static TrackPrioritizer GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new TrackPrioritizer();
+            }
+            return _instance;
+        }
+    }
+}
