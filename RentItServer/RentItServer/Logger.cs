153a38dff0658e95bd5e200ee2a1fb7eeb0e6d98
TheGreenDude <alu_cola@hotmail.com>
2013-03-01 12:48:36 +0100

    Created a logging class for the serber
    FileSystemHandler methods "Write" and "Read" implemented.

---
+++ RentItServer/RentItServer/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RentItServer
+{
+    public class Logger
+    {
+        private readonly string _fullPath;
+
+        public Logger(string fullPath)
+        {
+            fullPath = fullPath.Replace("\\", Path.DirectorySeparatorChar.ToString());
+            fullPath = fullPath.Replace("/", Path.DirectorySeparatorChar.ToString());
+            if (fullPath.EndsWith(Path.DirectorySeparatorChar.ToString())) throw new ArgumentException("Full must not target a directory. fullPath = " + fullPath);
+            
+            _fullPath = fullPath;
+
+            if (File.Exists(_fullPath) == false)
+            {
+                File.Create(_fullPath);
+            }
+        }
+
+        public void AddEntry(string log)
+        {
+            if (log == null) throw new ArgumentNullException("Log argument was null");
+            if (log.Equals("")) throw new ArgumentException("Log argument was empty");
+            string timeStamp = "["+DateTime.Now.ToString()+"] ";
+            File.AppendAllText(_fullPath, timeStamp + log);
+        }
+    }
+}
