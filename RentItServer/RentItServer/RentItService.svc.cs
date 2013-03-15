161ba5de06f3060d6851c730559e01b09066e64c
TheGreenDude <alu_cola@hotmail.com>
2013-03-15 13:52:40 +0100

    Added SMU interface

--- RentItServer/RentItServer/RentItService.svc.cs
+++ RentItServer/RentItServer/RentItService.svc.cs
@@ -6,12 +6,13 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 using RentItServer.ITU;
+using RentItServer.SMU;
 
 namespace RentItServer
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class RentItService : IRentItService
+    public class RentItService : IRentItService, IRentItServiceSMU
     {
         private static readonly Controller _controller = Controller.GetInstance();
 
