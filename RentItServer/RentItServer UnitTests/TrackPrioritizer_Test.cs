a3503e524f75817a484e09eb871e993f6dbde177
Andreas Precht Poulsen <dresen_precht@hotmail.com>
2013-03-15 15:15:32 +0100

    fixed references

--- RentItServer/RentItServer UnitTests/TrackPrioritizer_Test.cs
+++ RentItServer/RentItServer UnitTests/TrackPrioritizer_Test.cs
@@ -5,7 +5,7 @@ using RentItServer;
 using System.Collections.Generic;
 using Track = RentItServer.Track;
 using TrackPlay = RentItServer.TrackPlay;
-using TrackPrioritizer = RentItServer.ITU.TrackPrioritizer;
+using RentItServer.ITU;
 
 namespace RentItServer_UnitTests
 {
