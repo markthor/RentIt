dc9a5df5e61cc0d32355c8172970813bf82609d9
tokeloke <tokeloke@gmail.com>
2013-03-15 15:13:27 +0100

    contract content

--- RentItServer/RentItServer/SMURentItService.svc.cs
+++ RentItServer/RentItServer/SMURentItService.svc.cs
@@ -12,6 +12,44 @@ namespace RentItServer
     // NOTE: In order to launch WCF Test Client for testing this service, please select SMURentItService.svc or SMURentItService.svc.cs at the Solution Explorer and start debugging.
     public class SMURentItService : ISMURentItService
     {
-        
+        public int SignUp(string email, string name, string password)
+        {
+            throw new NotImplementedException("SingUp not implemented");
+        }
+
+        public int LogIn(string email, string password)
+        {
+            throw new NotImplementedException("LogIn not implemented");
+        }
+
+        public User GetUserInfo(int userId)
+        {
+            throw new NotImplementedException("GetUserInfo not implemented");
+        }
+
+        public bool UpdateUserInfo(int userId, string email, string name, string password)
+        {
+            throw new NotImplementedException("UpdateUserInfo not implemented");
+        }
+
+        public bool DeleteAccount(int userId)
+        {
+            throw new NotImplementedException("DeleteAccount not implemented");
+        }
+
+        public int HasRental(int userId, int bookId)
+        {
+            throw new NotImplementedException("HasRental not implemented");
+        }
+
+        public int RentBook(int userId, int bookId, DateTime startDate, int mediaType)
+        {
+            throw new NotImplementedException("RentBook not implemented");
+        }
+
+        public bool DeleteBook(int bookId)
+        {
+            throw new NotImplementedException("DeleteBook not implemented");
+        }
     }
 }
