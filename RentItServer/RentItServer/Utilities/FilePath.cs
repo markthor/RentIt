c76de042f252db379e3bbf07a5fe334bc5b1b2bf
Mark Thorhauge <markthorh@gmail.com>
2013-03-22 15:10:09 +0100

    added filepath

---
+++ RentItServer/RentItServer/Utilities/FilePath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace RentItServer.Utilities
+{
+    public sealed class FilePath
+    {
+        private string path;
+
+        public static readonly FilePath SMUFilePath = new FilePath("C:" + Path.DirectorySeparatorChar +
+            "Users" + Path.DirectorySeparatorChar +
+            "Rentit21" + Path.DirectorySeparatorChar +
+            "Documents" + Path.DirectorySeparatorChar +
+            "SMU" + Path.DirectorySeparatorChar +
+            "Files");
+
+        public static readonly FilePath ITUTrackPath = new FilePath("C:" + Path.DirectorySeparatorChar +
+            "Users" + Path.DirectorySeparatorChar +
+            "Rentit21" + Path.DirectorySeparatorChar +
+            "Documents" + Path.DirectorySeparatorChar +
+            "ITU" + Path.DirectorySeparatorChar +
+            "Tracks");
+
+        private FilePath(string path)
+        {
+            this.path = path;
+        }
+    }
+}
