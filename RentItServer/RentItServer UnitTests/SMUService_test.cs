13f7c6f8b4825b1e9332988fbb5682efe8dc47e7
tokeloke <tokeloke@gmail.com>
2013-03-22 14:41:11 +0100

    rentTest

--- RentItServer/RentItServer UnitTests/SMUService_test.cs
+++ RentItServer/RentItServer UnitTests/SMUService_test.cs
@@ -16,5 +16,14 @@ namespace RentItServer_UnitTests
 
             Assert.AreEqual(userId, id);
         }
+
+
+        [TestMethod]
+        public void AddBookTest_service()
+        {
+            ServiceReference1.SMURentItServiceClient service = new ServiceReference1.SMURentItServiceClient();
+            int bookId = service.UploadBook("bible", "God", "gods book", "religious", DateTime.Now, 1000.0);
+            int rentId = service.RentBook(1, bookId, DateTime.Now, 0);
+        }
     }
 }
