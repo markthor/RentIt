a608f0acdfc92568d47faba50d9badb7df134d72
Andreas Precht <dresen_precht@hotmail.com>
2013-03-25 14:18:00 +0100

    Corrected the code in FileName.cs

--- RentItServer/RentItServer/Utilities/FileName.cs
+++ RentItServer/RentItServer/Utilities/FileName.cs
@@ -10,12 +10,12 @@ namespace RentItServer.Utilities
     {
         public static string GeneratePdfFileName(int bookId)
         {
-            return String.Format("{0}PDF_BookId_{1}.pdf", Path.DirectorySeparatorChar, bookId.ToString());
+            return String.Format("PDF_BookId_{0}.pdf", bookId);
         }
 
         public static string GenerateAudioFileName(int bookId)
         {
-            return String.Format("{0}Audio_BookId_{1}.mp3", Path.DirectorySeparatorChar, bookId.ToString());
+            return String.Format("Audio_BookId_{0}.mp3", bookId);
         }
     }
 }
