a07c66de4592faf0b9e4886de371a80fe4f1fbef
TheGreenDude <alu_cola@hotmail.com>
2013-04-02 15:26:08 +0200

    .

--- RentItServer/RentItServer/SMURentItService.svc.cs
+++ RentItServer/RentItServer/SMURentItService.svc.cs
@@ -252,11 +252,6 @@ namespace RentItServer
             _smuController.UploadPDF(bookId, pdf);
         }
 
-        public void UploadImage(int bookId, MemoryStream image)
-        {
-            throw new NotImplementedException();
-        }
-
         /// <summary>
         /// Downloads the PDF for the book.
         /// </summary>
