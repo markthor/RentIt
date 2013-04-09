c0c35ed66afa38163a4e24025f7273cd595ab7c3
TheGreenDude <alu_cola@hotmail.com>
2013-04-09 16:25:19 +0200

    Did more stuff to our own dao

--- RentItServer/RentItServer/ITU/DAO.cs
+++ RentItServer/RentItServer/ITU/DAO.cs
@@ -31,6 +31,99 @@ namespace RentItServer.ITU
         }
 
         /// <summary>
+        /// Creates the user.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="email">The email.</param>
+        /// <returns>The id of the user.</returns>
+        public int CreateUser(string username, string password, string email)
+        {
+            using (RENTIT21Entities context = new RENTIT21Entities())
+            {
+                User theUser = new User()
+                    {
+                        Username = username,
+                        Password = password,
+                        //TODO: add email to entity
+                        Comments = new Collection<Comment>(),
+                        Channels = new Collection<Channel>(),
+                        SubscribedChannels = new Collection<Channel>(),
+                        Votes = new Collection<Vote>()
+                    };
+                context.Users.Add(theUser);
+                context.SaveChanges();
+
+                var users = from user in context.Users
+                            where user.Username.Equals(username) && user.Password.Equals(password)
+                            select user;
+                if (users.Any() == false)
+                {   // The user does not exist in the database, either something fucked up or another thread removed it already.
+                    throw new Exception("User got created and saved in the database but is not in the database.... O.ô.");
+                }
+                return users.First().Id;
+            }
+        }
+
+        /// <summary>
+        /// Removes the user from the database.
+        /// </summary>
+        /// <param name="userId">The user id.</param>
+        /// <exception cref="System.ArgumentException">No user with user id [+userId+]</exception>
+        /// <exception cref="System.Exception">End of \RemoveUser\. User was not get removed from the database.</exception>
+        public void DeleteUser(int userId)
+        {
+            using (RENTIT21Entities context = new RENTIT21Entities())
+            {
+                var users = from user in context.Users
+                            where user.Id == userId
+                            select user;
+                if (users.Any() == false) throw new ArgumentException("No user with user id [" + userId + "]");
+
+                User theUser = users.First();
+                context.Users.Remove(theUser);
+                context.SaveChanges();
+
+                users = from user in context.Users
+                        where user.Id == userId
+                        select user;
+
+                if (users.Any() == true) throw new Exception("End of \"RemoveUser\". User was not get removed from the database.");
+            }
+        }
+
+        /// <summary>
+        /// Updates the user.
+        /// </summary>
+        /// <param name="userId">The user id.</param>
+        /// <param name="username">The username. Can be null.</param>
+        /// <param name="password">The password. Can be null.</param>
+        /// <param name="channels">The channels. Can be null.</param>
+        /// <param name="comments">The comments. Can be null.</param>
+        /// <param name="subscribedChannels">The subscribed channels. Can be null.</param>
+        /// <param name="votes">The votes. Can be null.</param>
+        /// <exception cref="System.ArgumentException">No user with user id[ + userId + ]</exception>
+        public void UpdateUser(int userId, string username, string password, Collection<Channel> channels, Collection<Comment> comments, Collection<Channel> subscribedChannels, Collection<Vote> votes)
+        {
+            using (RENTIT21Entities context = new RENTIT21Entities())
+            {
+                var users = from user in context.Users
+                            where user.Id == userId
+                            select user;
+                if (users.Any() == false) throw new ArgumentException("No user with user id[" + userId + "]");
+
+                User theUser = users.First();
+                if (username != null) theUser.Username = username;
+                if (password != null) theUser.Password = password;
+                if (channels != null) theUser.Channels = channels;
+                if (comments != null) theUser.Comments = comments;
+                if (subscribedChannels != null) theUser.SubscribedChannels = subscribedChannels;
+                if (votes != null) theUser.Votes = votes;
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
         /// Creates a channel.
         /// </summary>
         /// <param name="channelName">Name of the channel.</param>
@@ -38,72 +131,136 @@ namespace RentItServer.ITU
         /// <param name="description">The description of the channel.</param>
         /// <param name="genres">The genres associated with the channel.</param>
         /// <returns>The created channel.</returns>
-        public Channel CreateChannel(string channelName, int userId, string description, string[] genres)
+        public Channel CreateChannel(string channelName, int userId, string description, IEnumerable<string> genres)
         {
-            // int channelId;
-            Channel ch;
             using (RENTIT21Entities context = new RENTIT21Entities())
             {
+                var users = from user in context.Users
+                            where user.Id == userId
+                            select user;
+                if (users.Any() == false)
+                {
+                    // throw new ArgumentException("No user with userId ["+userId+"]");
+                    throw new Exception();
+                }
+
                 var someGenres = from genre in context.Genres.Where(genre => genres.Contains(genre.Name))
                                  select genre;
-
-                var aUser = from user in context.Users.Where(user => user.Id == userId)
-                            select user;
+                if (someGenres.Any() == false)
+                {
+                    // throw new EmptyTableException("Genres");
+                    throw new Exception();
+                }
 
                 // Create the channel object
-                ch = new Channel()
+                Channel theChannel = new Channel()
                 {
                     Comments = new Collection<Comment>(),
                     Description = description,
                     Genres = someGenres.ToList(),
                     UserId = userId,
                     Name = channelName,
-                    ChannelOwner = aUser.First(),  
+                    ChannelOwner = users.First(),
                     Subscribers = new Collection<User>(),
                     Hits = null,
                     Rating = null,
                     Tracks = new Collection<Track>()
                 };
 
-                // I'm not sure this will work, i have not specified the channels id as the databse should generate it itself. 
-                context.Channels.Add(ch);
+                context.Channels.Add(theChannel);
                 context.SaveChanges();
 
-                //// Seems a bit weird way around this, but for now it's the deal :)
-                //var thelId = from channel in context.channels.Where(channel => channel.name == channelName && channel.userId == userId)
-                //                select channel.id;
+                var channels = from channel in context.Channels.Where(channel => channel.Name == channelName && channel.UserId == userId)
+                               select channel;
 
-                //if(thelId.Any() == true){
-                //    channelId = thelId.First();
-                //}
-                //else
-                //{   // The channel does not have an id in the database, either something fucked up or another thread removed it already.
-                //    throw new Exception("Channel got created and saved in the database but has no id.... O.ô.");
-                //}
-
-                var theChannel = from channel in context.Channels.Where(channel => channel.Name == channelName && channel.UserId == userId)
-                                 select channel;
-
-                if (theChannel.Any() == true){
-                    ch = theChannel.First();
+                if (channels.Any() == true)
+                {
+                    theChannel = channels.First();
                 }
                 else
                 {   // The channel does not exist in the database, either something fucked up or another thread removed it already.
-                   throw new Exception("Channel got created and saved in the database but is not in the database.... O.ô.");     
+                    throw new Exception("Channel got created and saved in the database but is not in the database.... O.ô.");
                 }
+                return theChannel;
             }
-
-            return ch;
         }
 
-        public int CreateUser(string username, string password, string email)
+        /// <summary>
+        /// Removes the channel from the database.
+        /// </summary>
+        /// <param name="ownerId">The owner id.</param>
+        /// <param name="theChannel">The channel.</param>
+        public void DeleteChannel(int ownerId, Channel theChannel)
         {
-            return -1;
+            using (RENTIT21Entities context = new RENTIT21Entities())
+            {
+                int channelId = theChannel.Id;
+                context.Channels.Remove(theChannel);
+                context.SaveChanges();
+
+                var channels = from channel in context.Channels
+                           where channel.ChannelOwner.Id == ownerId && channel.Id == channelId
+                           select channel;
+                //if(channels.Any() == true)  //TODO: throw exception here
+            }
         }
 
+        /// <summary>
+        /// Updates the channel.
+        /// </summary>
+        /// <param name="channelId">The channel id.</param>
+        /// <param name="ownerId">The owner id. Can be null.</param>
+        /// <param name="channelName">Name of the channel. Can be null.</param>
+        /// <param name="description">The description. Can be null.</param>
+        /// <param name="hits">The hits. Can be null.</param>
+        /// <param name="rating">The rating. Can be null.</param>
+        /// <param name="comments">The comments. Can be null.</param>
+        /// <param name="genres">The genres. Can be null.</param>
+        /// <param name="tracks">The tracks. Can be null.</param>
+        /// <exception cref="System.ArgumentException">
+        /// No channel with channel id [ + channelId + ]
+        /// or
+        /// No user with user id [ + ownerId + ]
+        /// </exception>
+        public void UpdateChannel(int channelId, int? ownerId, string channelName, string description, double? hits, double? rating, Collection<Comment> comments, Collection<Genre> genres, Collection<Track> tracks)
+        {
+            using (RENTIT21Entities context = new RENTIT21Entities())
+            {
+                var channels = from channel in context.Channels
+                               where channel.Id == channelId
+                               select channel;
+                if (channels.Any() == false) throw new ArgumentException("No channel with channel id [" + channelId + "]");
+
+                Channel theChannel = channels.First();
+                if (ownerId != null)
+                {
+                    var users = from user in context.Users
+                                where user.Id == ownerId
+                                select user;
+                    if (users.Any() == false) throw new ArgumentException("No user with user id [" + ownerId + "]");
+
+                    theChannel.UserId = (int)ownerId;
+                    theChannel.ChannelOwner = users.First();
+                }
+                if (channelName != null) theChannel.Name = channelName;
+                if (description != null) theChannel.Description = description;
+                if (hits != null) theChannel.Hits = (int)hits;
+                if (rating != null) theChannel.Rating = (double)rating;
+                if (comments != null) theChannel.Comments = comments;
+                if (genres != null) theChannel.Genres = genres;
+                if (tracks != null) theChannel.Tracks = tracks;
+
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Gets the channel.
+        /// </summary>
+        /// <param name="channelId">The channel id.</param>
+        /// <returns>The channel</returns>
         public Channel GetChannel(int channelId)
         {
-            Channel ch;
             using (RENTIT21Entities context = new RENTIT21Entities())
             {
                 var channels = from channel in context.Channels.Where(channel => channel.Id == channelId)
@@ -117,11 +274,14 @@ namespace RentItServer.ITU
                 {
                     // Da fuk?
                 }
-                ch = channels.First();
+                return channels.First();
             }
-            return ch;
         }
 
+        /// <summary>
+        /// Gets all channels.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Channel> GetAllChannels()
         {
             IEnumerable<Channel> allChannels;
@@ -207,39 +367,55 @@ namespace RentItServer.ITU
             return filteredChannels;
         }
 
-        public void DeleteChannel(int userId, int channelId)
+        /// <summary>
+        /// Creates a vote.
+        /// </summary>
+        /// <param name="rating">The rating.</param>
+        /// <param name="userId">The user id.</param>
+        /// <param name="trackId">The track id.</param>
+        /// <exception cref="System.ArgumentException">
+        /// no track with track id [+trackId+]
+        /// or
+        /// no user with user id [+userId+]
+        /// </exception>
+        public void CreateVote(int rating, int userId, int trackId)
         {
             using (RENTIT21Entities context = new RENTIT21Entities())
             {
-                var channels = from channel in context.Channels.Where(channel => channel.UserId == userId && channel.Id == channelId)
-                               select channel;
+                var tracks = from track in context.Tracks
+                             where track.Id == trackId
+                             select track;
 
-                if (channels.Any() == false)
-                {   // The channel does not exist
-                    return;
-                }
-                // Delete the channel entry from the database
-                context.Channels.Remove(channels.First());
-                context.SaveChanges();
-            }
-        }
+                if(tracks.Any() == false)   throw new ArgumentException("no track with track id ["+trackId+"]");
+                Track theTrack = tracks.First();
 
-        public void VoteTrack(int rating, int userId, int trackId)
-        {
-            using (RENTIT21Entities context = new RENTIT21Entities())
-            {   // TODO: Will only work if the vote table doesn't have "tracks" and "users" associated...
+                var users = from user in context.Users
+                            where user.Id == userId
+                            select user;
+
+                if(users.Any() == false)    throw new ArgumentException("no user with user id ["+userId+"]");
+                User theUser = users.First();
+
                 Vote vote = new Vote()
                     {
                         TrackId = trackId,
                         UserId = userId,
                         Value = rating,
-                        Date = DateTime.UtcNow
+                        Date = DateTime.UtcNow,
+                        User = theUser,
+                        Track = theTrack
                     };
                 context.Votes.Add(vote);
                 context.SaveChanges();
             }
         }
 
+        /// <summary>
+        /// Gets the track.
+        /// </summary>
+        /// <param name="trackId">The track id.</param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception">No track found with id =  + trackId</exception>
         public Track GetTrack(int trackId)
         {
             Track theTrack;
@@ -257,6 +433,10 @@ namespace RentItServer.ITU
             return theTrack;
         }
 
+        /// <summary>
+        /// Removes the track.
+        /// </summary>
+        /// <param name="track">The track.</param>
         public void RemoveTrack(Track track)
         {
             using (RENTIT21Entities context = new RENTIT21Entities())
@@ -266,17 +446,41 @@ namespace RentItServer.ITU
             }
         }
 
-        public void Comment(string comment, int userId, int channelId)
+        /// <summary>
+        /// Creates a comment.
+        /// </summary>
+        /// <param name="comment">The comment.</param>
+        /// <param name="userId">The user id.</param>
+        /// <param name="channelId">The channel id.</param>
+        /// <exception cref="System.ArgumentException">
+        /// No user with user id [+userId+]
+        /// or
+        /// No channel with channel id [ + channelId + ]
+        /// </exception>
+        public void CreateComment(string comment, int userId, int channelId)
         {
             using (RENTIT21Entities context = new RENTIT21Entities())
             {
-                // TODO: Will only work if the comment table doesn't have "channels" and "users" associated..
+                var users = from user in context.Users
+                            where user.Id == userId
+                            select user;
+                if(users.Any() == false)    throw new ArgumentException("No user with user id ["+userId+"]");
+                User theUser = users.First();
+
+                var channels = from channel in context.Channels
+                               where channel.Id == channelId
+                               select channel;
+                if (channels.Any() == false) throw new ArgumentException("No channel with channel id [" + channelId + "]");
+                Channel theChannel = channels.First();
+
                 Comment theComment = new Comment()
                     {
                         ChannelId = channelId,
                         Content = comment,
                         UserId = userId,
-                        Date = DateTime.UtcNow
+                        Date = DateTime.UtcNow,
+                        User = theUser,
+                        Channel = theChannel
                     };
                 context.Comments.Add(theComment);
                 context.SaveChanges();
