92040827b99edfd9b74b0d16f74cd7e85f65cdbf
Andreas Precht Poulsen <dresen_precht@hotmail.com>
2013-04-09 11:44:12 +0200

    Commented FileName.cs

--- RentItServer/RentItServer/Utilities/FileName.cs
+++ RentItServer/RentItServer/Utilities/FileName.cs
@@ -1,23 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
-
-namespace RentItServer.Utilities
+﻿namespace RentItServer.Utilities
 {
     public sealed class FileName
     {
+        /// <summary>
+        /// Generates a filename for the pdf for a book.
+        /// </summary>
+        /// <param name="bookId">The id of the book</param>
+        /// <returns>The filename of the pdf belonging to the book with the given bookId</returns>
         public static string GeneratePdfFileName(int bookId)
         {
             return string.Format("PDF_BookId_{0}.pdf", bookId);
         }
 
+        /// <summary>
+        /// Generates a filename for the audio for a book.
+        /// </summary>
+        /// <param name="bookId">The id of the book</param>
+        /// <returns>The filename of the audio belonging to the book with the given bookId</returns>
         public static string GenerateAudioFileName(int bookId)
         {
             return string.Format("Audio_BookId_{0}.mp3", bookId);
         }
 
+        /// <summary>
+        /// Generates a filename for the image for a book.
+        /// </summary>
+        /// <param name="bookId">The id of the book</param>
+        /// <returns>The filename of the image belonging to the book with the given bookId</returns>
         public static string GenerateImageFileName(int bookId)
         {
             return string.Format("Image_BookId_{0}.jpg", bookId);
