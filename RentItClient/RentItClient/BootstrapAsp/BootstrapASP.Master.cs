e42dfb1f8759c0cfb7447a2d1afda0c470021d17
tokeloke <tokeloke@gmail.com>
2013-04-09 14:02:29 +0200

    Bootstrap init

---
+++ RentItClient/RentItClient/BootstrapAsp/BootstrapASP.Master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BootstrapAsp
+{
+    public partial class BootstrapASP : System.Web.UI.MasterPage
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Page.Header.DataBind();
+        }
+    }
+}
