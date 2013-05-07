09f485974ecb7a9dc69c5e36f1cdb23e4c4b22fd
Mark Thorhauge <markthorh@gmail.com>
2013-05-07 12:31:42 +0200

    added streamhandlertest

---
+++ RentItServer/RentItServer UnitTests/ItuTests/StreamHandlerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RentItServer.ITU;
+using RentItServer;
+
+namespace RentItServer_UnitTests.ItuTests
+{
+    [TestClass]
+    public class StreamHandlerTest
+    {
+        [TestMethod]
+        public void TestStartStream()
+        {/*
+            User u = Controller.GetInstance().SignUp("Coolguy", "hattrick@hotmail.com", "12345");
+            Controller.GetInstance().CreateChannel("Pop Songs", u.Id, "Latest hits", 
+            StreamHandler.GetInstance().StartStream
+        */
+        }
+    }
+}
